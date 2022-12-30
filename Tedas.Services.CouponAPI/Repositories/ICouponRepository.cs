a608664cd8c5e8baed521876fab40d737bad62d6
bedranozcan <bedra@IDVLABS>
2022-12-30 15:55:22 +0300

    Coupon Project Added and Doing Migration

---
+++ Tedas.Services.CouponAPI/Repositories/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿using Tedas.Services.CouponAPI.Models.Dto;
+
+namespace Tedas.Services.CouponAPI.Repositories
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDto> GetCouponByCode(string couponCode);
+    }
+}
---
+++ Tedas.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tedas.MessageBus;
+using Tedas.Services.ShoppingCartAPI.Messages;
+using Tedas.Services.ShoppingCartAPI.Models.Dto;
+using Tedas.Services.ShoppingCartAPI.RabbitMQSender;
+using Tedas.Services.ShoppingCartAPI.Repository;
+
+namespace Tedas.Services.ShoppingCartAPI.Controllers
+{
+    [ApiController]
+    [Route("api/cart")]
+    public class CartAPIController : Controller
+    {
+        private readonly ICartRepository _cartRepository;
+        private readonly ICouponRepository _couponRepository;
+        // private readonly IMessageBus _messageBus;
+        protected ResponseDto _response;
+        private readonly IRabbitMQMessageSender _rabbitMQMessageSender;
+
+        public CartAPIController(ICartRepository cartRepository, //IMessageBus messageBus,
+            ICouponRepository couponRepository, IRabbitMQMessageSender rabbitMQMessageSender)
+        {
+            _cartRepository = cartRepository;
+            _couponRepository = couponRepository;
+            _rabbitMQMessageSender = rabbitMQMessageSender;
+            // _messageBus = messageBus;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet("GetCart/{userId}")]
+        public async Task<object> GetCart(string userId)
+        {
+            try
+            {
+                CartDto cartDto = await _cartRepository.GetCartByUserId(userId);
+                _response.Result = cartDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("AddCart")]
+        public async Task<object> AddCart(CartDto cartDto)
+        {
+            try
+            {
+                CartDto cartDt = await _cartRepository.CreateUpdateCart(cartDto);
+                _response.Result = cartDt;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("UpdateCart")]
+        public async Task<object> UpdateCart(CartDto cartDto)
+        {
+            try
+            {
+                CartDto cartDt = await _cartRepository.CreateUpdateCart(cartDto);
+                _response.Result = cartDt;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("RemoveCart")]
+        public async Task<object> RemoveCart([FromBody] int cartId)
+        {
+            try
+            {
+                bool isSuccess = await _cartRepository.RemoveFromCart(cartId);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("ApplyCoupon")]
+        public async Task<object> ApplyCoupon([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                bool isSuccess = await _cartRepository.ApplyCoupon(cartDto.CartHeader.UserId,
+                    cartDto.CartHeader.CouponCode);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("RemoveCoupon")]
+        public async Task<object> RemoveCoupon([FromBody] string userId)
+        {
+            try
+            {
+                bool isSuccess = await _cartRepository.RemoveCoupon(userId);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("Checkout")]
+        public async Task<object> Checkout(CheckoutHeaderDto checkoutHeader)
+        {
+            try
+            {
+                CartDto cartDto = await _cartRepository.GetCartByUserId(checkoutHeader.UserId);
+                if (cartDto == null)
+                {
+                    return BadRequest();
+                }
+
+                if (!string.IsNullOrEmpty(checkoutHeader.CouponCode))
+                {
+                    CouponDto coupon = await _couponRepository.GetCoupon(checkoutHeader.CouponCode);
+                    if (checkoutHeader.DiscountTotal != coupon.DiscountAmount)
+                    {
+                        _response.IsSuccess = false;
+                        _response.ErrorMessages = new List<string>() { "Coupon Price has changed, please confirm" };
+                        _response.DisplayMessage = "Coupon Price has changed, please confirm";
+                        return _response;
+                    }
+                }
+
+                checkoutHeader.CartDetails = cartDto.CartDetails;
+                //logic to add message to process order.
+                // await _messageBus.PublishMessage(checkoutHeader, "checkoutqueue");
+
+                ////rabbitMQ
+                _rabbitMQMessageSender.SendMessage(checkoutHeader, "checkoutqueue");
+                await _cartRepository.ClearCart(checkoutHeader.UserId);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
